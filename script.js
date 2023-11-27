cec5a13cbfab6bffee4447cde6730f49f9977191
Hassan Sohail <124749090+hsohail7@users.noreply.github.com>
2023-11-27 18:05:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// script.js
-// You can add JavaScript code here to make your website interactive
+<script>
+        // JavaScript function to play the video when poster image is clicked
+        function playVideo() {
+            var video = document.getElementById('hackathon-video');
+            var poster = document.querySelector('.video-poster');
+            video.style.display = 'block';
+            poster.style.display = 'none';
+            video.play();
+        }
+    </script>
