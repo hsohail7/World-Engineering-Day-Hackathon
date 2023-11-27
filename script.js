2ad0533e5aa1d5a713230e973698a24764ff6c55
Hassan Sohail <124749090+hsohail7@users.noreply.github.com>
2023-11-27 18:08:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 <script>
-        // JavaScript function to play the video when poster image is clicked
-        function playVideo() {
-            var video = document.getElementById('hackathon-video');
-            var poster = document.querySelector('.video-poster');
-            video.style.display = 'block';
-            poster.style.display = 'none';
-            video.play();
-        }
-    </script>
+    // JavaScript function to play the video when poster image is clicked
+    function playVideo() {
+        var video = document.getElementById('video-container'); // Updated ID
+        var poster = document.querySelector('.video-poster');
+        video.style.display = 'block';
+        poster.style.display = 'none';
+        video.play();
+    }
+</script>
